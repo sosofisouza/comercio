f872eaba8e9bf68796a75267086cd7a6375fdf25
Sofia Souza <sofiasouza0512@gmail.com>
2023-06-07 07:35:28 -0300

    Inicio controle de versão

---
+++ src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
@@ -0,0 +1,15 @@
+package com.itb.inf2fm.comercio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/comercio/produtos")
+public class LojaController {
+      
+	@GetMapping("/listar")
+	public void listarProdutos() {
+	  System.out.println("LISTA DE PRODUTOS");
+	}
+}
---
+++ src/main/java/com/itb/inf2fm/comercio/model/Produto.java
@@ -0,0 +1,14 @@
+package com.itb.inf2fm.comercio.model;
+
+public class Produto {
+
+	private Long id;
+	private String nome; 
+	private String descricao;
+	private String codigoBarras;
+	private double preco;
+	
+	//public: Acesso liberado para todas as classes.
+	//private: Acesso permitido apenas para os membros da própria classe 
+	//Entende-de por membros(atributos e ou métodos)
+}
