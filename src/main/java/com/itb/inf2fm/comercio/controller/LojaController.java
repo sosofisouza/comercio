fe9d334386f8c574af1f80dfa89c7ce19e88d71e
Sofia Souza <sofiasouza0512@gmail.com>
2023-06-26 08:26:46 -0300

    Continuação da edição de listagem de produtos font-end

--- src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,7 +17,7 @@ public class LojaController {
 	List<Produto> listaDeProdutos = new ArrayList<Produto>();
       
 	@GetMapping("/listar")
-	public String listarProdutos() {
+	public String listarProdutos(Model model) {
 		
 		Produto p1 = new Produto();
 		p1.setId(20l);
@@ -36,7 +37,10 @@ public class LojaController {
 		listaDeProdutos.add(p1);
 		listaDeProdutos.add(p2);
 		
+		model.addAttribute("listaDeProdutos", listaDeProdutos);
+		return "produtos";
 		
-	 return "produtos";
+		
+	  
 	}
 }
